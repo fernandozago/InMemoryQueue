a072d917cdee39b2371a80362fd471a3b50514ff
Fernando Zago <fe.zago@live.com>
2022-12-15 15:37:40 -0300

    Add log information to background example

--- Examples/Hosts/GrpcService2/InMemoryConsumers/InMemoryDefaultQueueConsumerBackground.cs
+++ Examples/Hosts/GrpcService2/InMemoryConsumers/InMemoryDefaultQueueConsumerBackground.cs
@@ -7,16 +7,22 @@ namespace GrpcService2.Services
     public class InMemoryDefaultQueueConsumerBackground : BackgroundService
     {
         private readonly InMemoryQueueManager _inMemoryQueueManager;
+        private readonly ILogger<InMemoryDefaultQueueConsumerBackground> _logger;
 
-        public InMemoryDefaultQueueConsumerBackground(InMemoryQueueManager inMemoryQueueManager)
+        public InMemoryDefaultQueueConsumerBackground(InMemoryQueueManager inMemoryQueueManager, ILogger<InMemoryDefaultQueueConsumerBackground> logger)
         {
             _inMemoryQueueManager = inMemoryQueueManager;
+            _logger = logger;
         }
 
         protected override Task ExecuteAsync(CancellationToken stoppingToken) =>
             _inMemoryQueueManager.CreateInMemoryConsumer(ProcessItem, nameof(InMemoryDefaultQueueConsumerBackground), "InMemoryConsumerQueue", stoppingToken);
 
-        private Task<bool> ProcessItem(QueueItem queueItem) =>
-            Task.FromResult(true);
+        private Task<bool> ProcessItem(QueueItem queueItem)
+        {
+            _logger.LogInformation(queueItem.Message);
+            return Task.FromResult(true);
+        }
+            
     }
 }
