07c8466e184ec4bee1a7615f4370800c3a6a1c60
Fernando Zago <fe.zago@live.com>
2023-01-16 14:30:00 -0300

    - Code Review

--- MemoryQueue.Base/InMemoryQueueInfoService.cs
+++ MemoryQueue.Base/InMemoryQueueInfoService.cs
@@ -7,14 +7,14 @@ namespace MemoryQueue.Base
     {
         private readonly SemaphoreSlim _semaphore = new (1);
         private readonly InMemoryQueue _inMemoryQueue;
-        private QueueInfo _queueInfo = new QueueInfo();
+        private QueueInfo _queueInfo;
         private int _lastGeneratedSecond;
 
         public InMemoryQueueInfo(InMemoryQueue inMemoryQueue)
         {
             _inMemoryQueue = inMemoryQueue;
             _lastGeneratedSecond = new TimeSpan(Stopwatch.GetTimestamp()).Seconds;
-            InternalSetQueueInfo();
+            _queueInfo = InternalGetQueueInfo();
         }
 
         public QueueInfo GetQueueInfo()
@@ -23,68 +23,69 @@ namespace MemoryQueue.Base
             if (_lastGeneratedSecond != second && _semaphore.Wait(0))
             {
                 _lastGeneratedSecond = second;
-                InternalSetQueueInfo();
+                _queueInfo = InternalGetQueueInfo();
                 _semaphore.Release();
             }
-
-            return _queueInfo!;
+            return _queueInfo;
         }
 
-        private void InternalSetQueueInfo()
+        private QueueInfo InternalGetQueueInfo()
         {
+            var collectDate = DateTime.Now;
             int mainQueueSize = _inMemoryQueue.MainChannelCount;
             int retryQueueSize = _inMemoryQueue.RetryChannelCount;
+            var queueInfo = new QueueInfo()
+            {
+                CollectDate = collectDate,
+                QueueName = _inMemoryQueue.Name,
+                QueueSize = mainQueueSize + retryQueueSize,
+                MainQueueSize = mainQueueSize,
+                RetryQueueSize = retryQueueSize,
 
-            _queueInfo.CollectDate = DateTime.Now;
-            _queueInfo.QueueName = _inMemoryQueue.Name;
-            _queueInfo.QueueSize = mainQueueSize + retryQueueSize;
-            _queueInfo.MainQueueSize = mainQueueSize;
-            _queueInfo.RetryQueueSize = retryQueueSize;
+                ConcurrentConsumers = _inMemoryQueue.ConsumersCount,
 
-            _queueInfo.ConcurrentConsumers = _inMemoryQueue.ConsumersCount;
+                AckCounter = _inMemoryQueue.Counters.AckCounter,
+                AckPerSecond = _inMemoryQueue.Counters.AckPerSecond,
 
-            _queueInfo.AckCounter = _inMemoryQueue.Counters.AckCounter;
-            _queueInfo.AckPerSecond = _inMemoryQueue.Counters.AckPerSecond;
+                NackCounter = _inMemoryQueue.Counters.NackCounter,
+                NackPerSecond = _inMemoryQueue.Counters.NackPerSecond,
 
-            _queueInfo.NackCounter = _inMemoryQueue.Counters.NackCounter;
-            _queueInfo.NackPerSecond = _inMemoryQueue.Counters.NackPerSecond;
+                PubCounter = _inMemoryQueue.Counters.PubCounter,
+                PubPerSecond = _inMemoryQueue.Counters.PubPerSecond,
 
-            _queueInfo.PubCounter = _inMemoryQueue.Counters.PubCounter;
-            _queueInfo.PubPerSecond = _inMemoryQueue.Counters.PubPerSecond;
+                RedeliverCounter = _inMemoryQueue.Counters.RedeliverCounter,
+                RedeliverPerSecond = _inMemoryQueue.Counters.RedeliverPerSecond,
 
-            _queueInfo.RedeliverCounter = _inMemoryQueue.Counters.RedeliverCounter;
-            _queueInfo.RedeliverPerSecond = _inMemoryQueue.Counters.RedeliverPerSecond;
+                DeliverCounter = _inMemoryQueue.Counters.DeliverCounter,
+                DeliverPerSecond = _inMemoryQueue.Counters.DeliverPerSecond,
 
-            _queueInfo.DeliverCounter = _inMemoryQueue.Counters.DeliverCounter;
-            _queueInfo.DeliverPerSecond = _inMemoryQueue.Counters.DeliverPerSecond;
-
-            _queueInfo.AvgAckTimeMilliseconds = _inMemoryQueue.Counters.AvgConsumptionMs;
-
-            _queueInfo.Consumers.Clear();
-            _queueInfo.Consumers.AddRange(_inMemoryQueue.Consumers.Select(ParseConsumer));
+                AvgAckTimeMilliseconds = _inMemoryQueue.Counters.AvgConsumptionMs
+            };
+            queueInfo.Consumers.AddRange(_inMemoryQueue.Consumers.Select(ParseConsumer));
+            return queueInfo;
         }
 
         private static ConsumerInfo ParseConsumer(QueueConsumerInfo info)
         {
-            var _consumerInfo = new ConsumerInfo();
-            _consumerInfo.Counters ??= new Models.Counters();
+            var consumerInfo = new ConsumerInfo();
+            consumerInfo.Counters ??= new Models.Counters();
 
-            _consumerInfo.Host = info.Host;
-            _consumerInfo.Id = info.Id;
-            _consumerInfo.Ip = info.Ip;
-            _consumerInfo.Name = info.Name;
-            _consumerInfo.Type = info.ConsumerType.ToString();
+            consumerInfo.Host = info.Host;
+            consumerInfo.Id = info.Id;
+            consumerInfo.Ip = info.Ip;
+            consumerInfo.Name = info.Name;
+            consumerInfo.Type = info.ConsumerType.ToString();
 
-            _consumerInfo.Counters.AckCounter = info.Counters?.AckCounter ?? 0;
-            _consumerInfo.Counters.AckPerSecond = info.Counters?.AckPerSecond ?? 0;
-            _consumerInfo.Counters.AvgConsumptionMs = info.Counters?.AvgConsumptionMs ?? 0;
-            _consumerInfo.Counters.DeliverCounter = info.Counters?.DeliverCounter ?? 0;
-            _consumerInfo.Counters.DeliverPerSecond = info.Counters?.DeliverPerSecond ?? 0;
-            _consumerInfo.Counters.NackCounter = info.Counters?.NackCounter ?? 0;
-            _consumerInfo.Counters.NackPerSecond = info.Counters?.NackPerSecond ?? 0;
-            _consumerInfo.Counters.Throttled = info.Counters?.Throttled ?? false;
+            consumerInfo.Counters.AckCounter = info.Counters?.AckCounter ?? 0;
+            consumerInfo.Counters.AckPerSecond = info.Counters?.AckPerSecond ?? 0;
+            consumerInfo.Counters.AvgConsumptionMs = info.Counters?.AvgConsumptionMs ?? 0;
+            consumerInfo.Counters.DeliverCounter = info.Counters?.DeliverCounter ?? 0;
+            consumerInfo.Counters.DeliverPerSecond = info.Counters?.DeliverPerSecond ?? 0;
+            consumerInfo.Counters.NackCounter = info.Counters?.NackCounter ?? 0;
+            consumerInfo.Counters.NackPerSecond = info.Counters?.NackPerSecond ?? 0;
+            consumerInfo.Counters.Throttled = info.Counters?.Throttled ?? false;
 
-            return _consumerInfo;
+            return consumerInfo;
         }
     }
 }
