c5e1751b0d71772e580f2fcd0d6a9d5e98c53a4b
Fernando Zago <fe.zago@live.com>
2023-01-18 14:32:11 -0300

    - Fixed Project Dependencies for SignalR Packages

--- MemoryQueue.Client.SignalR/QueueItemReply.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MemoryQueue.Transports.SignalR
-{
-    public class QueueItemReply
-    {
-        public string Message { get; set; }
-        public bool Retrying { get; set; }
-        public int RetryCount { get; set; }
-    }
-}
