c9e21d063bc075a0e224e19ad667aff4435c6b0a
Fernando Zago <fe.zago@live.com>
2023-01-18 15:22:42 -0300

    - Simplifying SubjectUnderTestFactory

--- MemoryQueue.Tests/SUTFactory/SubjectUnderTestFactory.cs
+++ MemoryQueue.Tests/SUTFactory/SubjectUnderTestFactory.cs
@@ -1,7 +1,6 @@
 ﻿using MemoryQueue.Base;
 using MemoryQueue.GRPC.Transports.GRPC.Services;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Moq;
 
@@ -9,17 +8,10 @@ namespace MemoryQueue.Tests.SUTFactory
 {
     public static class SubjectUnderTestFactory
     {
-        public static InMemoryQueueManager CreateInMemoryQueueManager()
-        {
-            return new(new NullLoggerFactory());
-        }
+        public static InMemoryQueueManager CreateInMemoryQueueManager() =>
+            new(new NullLoggerFactory());
 
-        public static ConsumerServiceImpl CreateGrpcConsumer()
-        {
-            var loggerFactory = new NullLoggerFactory();
-            Mock<IConfiguration> configurationMock = new();
-            InMemoryQueueManager queueManager = new(loggerFactory);
-            return new ConsumerServiceImpl(queueManager, configurationMock.Object, loggerFactory);
-        }
+        public static ConsumerServiceImpl CreateGrpcConsumer() =>
+            new (CreateInMemoryQueueManager(), new Mock<IConfiguration>().Object, new NullLoggerFactory());
     }
 }
