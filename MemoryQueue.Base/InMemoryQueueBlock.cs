532366aebe33a33351fb301914dd77e562d3f144
Fernando Zago <fe.zago@live.com>
2023-01-19 12:05:43 -0300

    - Added Comments on InMemoryQueueBlock - Explain OfferMessage (Interception) - Explain ProcessItem (After message was offered and accepted by the ActionBlock)

--- MemoryQueue.Base/InMemoryQueueBlock.cs
+++ MemoryQueue.Base/InMemoryQueueBlock.cs
@@ -38,16 +38,33 @@ namespace MemoryQueue.Base
         public void Fault(Exception exception) => 
             _actionBlock.Fault(exception);
 
+        /// <summary>
+        /// Intercepts a message before trying to offer to a ActionBlock
+        /// Checks the CancellationToken, if is Cancelled, call Complete() and return DecliningPermanently 
+        /// to avoid new messages to be offered to this ITargetBlock
+        /// </summary>
+        /// <param name="messageHeader"></param>
+        /// <param name="messageValue"></param>
+        /// <param name="source"></param>
+        /// <param name="consumeToAccept"></param>
+        /// <returns></returns>
         public DataflowMessageStatus OfferMessage(DataflowMessageHeader messageHeader, QueueItem messageValue, ISourceBlock<QueueItem>? source, bool consumeToAccept)
         {
             if (_token.IsCancellationRequested)
             {
-                _actionBlock.Complete();
+                Complete();
                 return DataflowMessageStatus.DecliningPermanently;
             }
             return _actionBlock.OfferMessage(messageHeader, messageValue, source, consumeToAccept);
         }
 
+        /// <summary>
+        /// Already received an item to process in ActionBlock
+        /// Proceed trying to send the message to Consumer and after that, validate again the CancellationToken
+        /// If cancelled, call Complete(), otherwise keep it active
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
         private async Task ProcessItem(QueueItem item)
         {
             if (!(await _action(item).ConfigureAwait(false)))
@@ -58,7 +75,7 @@ namespace MemoryQueue.Base
             if (_token.IsCancellationRequested)
             {
                 Console.WriteLine("Completing ActionBlock");
-                _actionBlock.Complete();
+                Complete();
             }
         }
 
